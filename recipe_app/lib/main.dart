ec6b70d018d5c10343f6e37f3a541076289af4c5
Harshita <harshita@sample.com>
2024-01-24 15:41:06 +0530

    change

--- recipe_app/lib/main.dart
+++ recipe_app/lib/main.dart
@@ -78,12 +78,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
