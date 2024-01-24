c29ad8de30885c2c39dcb97977129d88fc1326fa
HARSHITA <114668133+H-a-r-s-h-i-t-a-k@users.noreply.github.com>
2024-01-24 15:44:10 +0530

    change

--- recipe_app/lib/main.dart
+++ recipe_app/lib/main.dart
@@ -13,8 +13,7 @@ class Recipe_App extends StatelessWidget {
     return MaterialApp(
       title: 'Recipe App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
+        
         // TRY THIS: Try running your application with "flutter run". You'll see
         // the application has a purple toolbar. Then, without quitting the app,
         // try changing the seedColor in the colorScheme below to Colors.green
