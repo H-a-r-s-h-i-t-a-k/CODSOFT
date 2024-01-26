61827f6c633a7ba63b15134fb5db87eef43df6f3
HARSHITA <114668133+H-a-r-s-h-i-t-a-k@users.noreply.github.com>
2024-01-27 02:49:51 +0530

    Update main.dart

--- recipe_app/lib/main.dart
+++ recipe_app/lib/main.dart
@@ -15,7 +15,7 @@ class Recipe_App extends StatelessWidget {
       theme: ThemeData(
         
     
-        // try changing the seedColor in the colorScheme below to Colors.green
+       
         // and then invoke "hot reload" (save your changes or press the "hot
         // reload" button in a Flutter-supported IDE, or press "r" if you used
         // the command line to start the app).
